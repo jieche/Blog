88127b891f6b2b213c5a0f92aece9bc34a9cdb81
Lin <2992738698@qq.com>
2022-08-07 15:25:43 +0800

    http://localhost/home/default.html 展示静态页面

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //引用express框架
 const express = require('express');
+//处理路径
+const path=require('path');
+
 //创建网站服务器
 const app=express();
 
+//开放静态资源文件
+app.use(express.static(path.join(__dirname,'public')));
+
+//引入路由模块
 const home =require('./route/home');
 const admin =require('./route/admin');
 
